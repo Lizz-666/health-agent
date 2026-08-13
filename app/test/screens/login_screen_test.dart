382802bcf6529c34dc06f8f553e53bebf334a18c
Lizz <2487562043@qq.com>
2026-08-13 13:35:11 +0800

    test(phase9): keep login gate analysis clean

--- app/test/screens/login_screen_test.dart
+++ app/test/screens/login_screen_test.dart
@@ -18,7 +18,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:posture_app/core/api_client.dart';
-import 'package:posture_app/providers/auth_provider.dart';
 import 'package:posture_app/screens/auth/login_screen.dart';
 
 Widget _wrap() => const ProviderScope(child: MaterialApp(home: LoginScreen()));
